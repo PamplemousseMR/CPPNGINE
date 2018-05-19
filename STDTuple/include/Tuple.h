9f0a631ff049e335942003b9836017452c2642d3
pamplemousse26 <romain.manciaux@gmail.com>
2018-05-19 21:38:40 +0200

    feat(Tuple): first tuple example

---
+++ STDTuple/include/Tuple.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <ostream>
+
+/*
+    this class test std::tuple
+*/
+template<typename... T>
+class Tuple;
+
+template<>
+class Tuple<>
+{
+
+public:
+
+    Tuple();
+
+    std::ostream& operator <<(std::ostream&) const;
+
+};
+
+template<typename T, typename... R>
+class Tuple<T, R...> : private Tuple<R...>
+{
+
+public:
+
+    template<std::size_t S>
+    struct Evaluator{};
+
+    Tuple(T, R...);
+
+    std::ostream& operator <<(std::ostream&) const;
+
+    friend std::ostream& operator <<(std::ostream& _o, const Tuple<T, R...>& _t)
+    {
+        return _t.operator <<(_o);
+    }
+
+private:
+
+    T m_this;
+
+};
+
---
+++ STDTuple/src/Main.cpp
@@ -0,0 +1,11 @@
+#include "Tuple.h"
+#include "Tuple.hxx"
+
+#include <iostream>
+
+int main()
+{
+    Tuple<int, float, int, bool> t(1, 1.2f, 1, true);
+
+	return 0;
+}
