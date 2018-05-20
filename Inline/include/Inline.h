b7de19c725419c531951dbcd81b17227b945d28f
pamplemousse26 <romain.manciaux@gmail.com>
2018-05-20 23:18:59 +0200

    feat(Inline): add first exemple

---
+++ Inline/include/Inline.h
@@ -0,0 +1,6 @@
+#pragma once
+
+inline int abs(int _i)
+{
+    return _i >= 0 ? _i : -_i;
+}
---
+++ Inline/src/Main.cpp
@@ -0,0 +1,13 @@
+#include "Inline.h"
+
+#include <assert.h>
+
+/*
+    this project test inline keyword
+*/
+int main()
+{
+    assert(abs(-1) == 1);
+
+    return 0;
+}
